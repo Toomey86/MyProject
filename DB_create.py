d5f655e3b18b517ea7c30e948aa38ad9a7056977
Martin Toomey <c06427715@mydit.ie>
2019-03-28 22:57:12 +0000

    added DB_create to create database remotely

--- DB_create.py
+++ DB_create.py
@@ -3,6 +3,8 @@ from models import User
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+set FLASK_APP=app.py
+
 db.create_all()
 
 admin = User(firstname='Martin', lastname='Toomey', email='martin.toomey86@gmail.com', phone='0879126382', role=0,manager_id=None, manager=None, password='password')
