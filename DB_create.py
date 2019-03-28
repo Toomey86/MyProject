03088c1a433c523fd6eede592b9bf44b3f5b9f71
Martin Toomey <c06427715@mydit.ie>
2019-03-28 23:06:06 +0000

    added DB_create to create database remotely

--- DB_create.py
+++ DB_create.py
@@ -3,11 +3,16 @@ from models import User
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-set FLASK_APP=app.py
+# FLASK_APP(app.py)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/blah'
+db.init_app(app)
+app.secret_key = "development-key"
+db = SQLAlchemy(app)
+Migrate(app,db)
+
 
 db.create_all()
-
 admin = User(firstname='Martin', lastname='Toomey', email='martin.toomey86@gmail.com', phone='0879126382', role=0,manager_id=None, manager=None, password='password')
 db.session.add(admin)
-
 db.session.commit()
