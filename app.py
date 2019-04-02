47a15af0ba315ea81a8a85656654b4b6f159c8be
Martin Toomey <c06427715@mydit.ie>
2019-04-02 23:30:43 +0100

    3rd

--- app.py
+++ app.py
@@ -339,7 +339,7 @@ def settings():
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
     #app.run(host='0.0.0.0')
 	#app.run(host='0.0.0.0',port=5000)
     #app.run(host='127.0.0.1',port=5000)
